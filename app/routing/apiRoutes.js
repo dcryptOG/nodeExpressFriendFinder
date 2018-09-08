f15b3fcee59a225b4fde7dbc1b2c8a1dc17e0fc9
unknown <dcryptOG.geoffdotson@gmail.com>
2018-09-08 08:40:04 -0500

    more better

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,36 +1,30 @@
-var path = require("path");
-var friends = require("../data/friends");
+// dependencies 
+const path = require("path");
+const friends = require("../data/friends");
 
 module.exports = function(app){
 
-    app.get('/api/friends', function(req, res){
+    app.get('/api/friends', (req, res) => {
         console.log("Reading API");
         res.json(friends);
     });
 
     app.post("/api/new", function(req, res) {
-      //setup variables for finding match
+      //variables for finding match
         var newFriend = req.body;
+        console.log(newFriend);
         var newScore = newFriend.scores;
+        console.log(newScore);
         var total = 0;
-        var bestMatch = 1000;
-        var index = -1;
 
         for(var i = 0; i < friends.length; i++){
+            return res.json(friends[index]);
             //Iterate through the whole list of friends already in database
             total = 0;
-
-            for(var j = 0; j < newScore.length; j++){
-                //for each friend calculate the total value
                 var diff = Math.abs(newScore[j] - friends[i].scores[j]);
                 total += diff;
             }
-            if(total < bestMatch){
-                bestMatch = total;
-                index = i;
-            }
-        }
-        console.log('Best Match:', friends[index]);
+            
         friends.push(newFriend);
         res.json(friends[index]);
     });
