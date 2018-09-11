c1051a2c9652e95c7a433dd444622ead46ee210b
unknown <dcryptOG.geoffdotson@gmail.com>
2018-09-10 20:15:26 -0500

    Test css

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,10 @@
  Your `htmlRoutes.js` file should include two routes
     A GET Route to `/survey` which should display the survey page.
     A default, catch-all route that leads to `home.html` which displays the home page.   
-*/ 
+*/
 const path = require('path');
 
-module.exports = function(app) {
+module.exports = function (app) {
 	// directs user to the survey HTML if survey domain is used or button press
 	app.get('/survey', (req, res) => {
 		res.sendFile(path.join(__dirname, '/../public/survey.html'));
--- server.js
+++ server.js
@@ -1,34 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+// Dependencies
+// =============================================================
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 
+// Sets up the Express App
+// =============================================================
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 8080;
-app.use(express.static(__dirname + 'app'));
-// "C:/Users/Geoff/Desktop/class/homework/nodeExpressFriendFinder/app/public/assets/css/style.css"
-// app.use('/static', express.static(path.join(__dirname, 'public')))
-// console.log(('/static', express.static(path.join(__dirname, 'app/public/assets/css'))));
-// app.use(express.static(__dirname + "\\app\\public\\assets\\css\\style.css"));
-// app.use(express.static(__dirname + "\\app\\public\\assets\\js\\app.js"));
-// console.log(__dirname + "\\app\\public\\assets\\css\\style.css");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+
+// Static files
+app.use(express.static('app/public'));
+
+
+// Router
+require('./app/routing/apiRoutes.js')(app, path);
+require('./app/routing/htmlRoutes.js')(app, path);
+
 
 app.listen(PORT, () => {
-	console.log("App listening on PORT: " + PORT);
-});
-
-
-/*
-steps 
-
-Edit htmlRoutes
-
-edit htmlRoutes & use loop from star wars
-*/
+	console.log(`I\'m listening... on port " + ${PORT}`);
+})
