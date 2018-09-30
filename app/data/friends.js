173735e5ae614dc15efde00c8113b92a2a7b9a90
unknown <dcryptOG.geoffdotson@gmail.com>
2018-09-30 16:45:16 -0500

    Got name score pic popup

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,31 @@
-
 // array of freinds pervert
 var friends = [{
   name: "Candy Cummings",
-  photo: "",
+  photo: "https://edge.alluremedia.com.au/uploads/businessinsider/2018/03/bezos-iguana.jpg",
   scores: [9, 5, 7, 7, 1, 7, 5, 1, 7, 1]
 }, {
   name: "Laura Toner",
-  photo: "",
+  photo: "https://edge.alluremedia.com.au/uploads/businessinsider/2018/03/bezos-iguana.jpg",
   scores: [1, 5, 9, 7, 5, 9, 5, 9, 1, 7]
 }, {
   name: "Chrissy Boner",
-  photo: "",
+  photo: "https://edge.alluremedia.com.au/uploads/businessinsider/2018/03/bezos-iguana.jpg",
   scores: [7, 9, 9, 1, 5, 1, 5, 7, 1, 5]
-},{
+}, {
   name: "Dr. Milk Dick Phd",
-  photo: "",
+  photo: "https://edge.alluremedia.com.au/uploads/businessinsider/2018/03/bezos-iguana.jpg",
   scores: [5, 1, 9, 9, 7, 5, 1, 5, 9, 9]
 }, {
   name: "Bi-Curious George",
-  photo: "",
+  photo: "https://edge.alluremedia.com.au/uploads/businessinsider/2018/03/bezos-iguana.jpg",
   scores: [5, 7, 5, 9, 1, 7, 1, 5, 9, 1]
 }, {
   name: "Tanner Cox",
-  photo: "",
+  photo: "https://edge.alluremedia.com.au/uploads/businessinsider/2018/03/bezos-iguana.jpg",
   scores: [1, 5, 5, 1, 5, 7, 9, 9, 5, 7]
 }, {
   name: "Dick Pound",
-  photo: "",
+  photo: "https://edge.alluremedia.com.au/uploads/businessinsider/2018/03/bezos-iguana.jpg",
   scores: [1, 1, 5, 9, 7, 1, 5, 9, 9, 7]
 }];
 
