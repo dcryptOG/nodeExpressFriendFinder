8775ca07226e719cc753d1bcb2c476c9ade05e38
unknown <dcryptOG.geoffdotson@gmail.com>
2018-09-30 21:49:14 -0500

    Fixed script tags

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -1,20 +1,13 @@
 window.onload = function () {
-
-
     var userName = document.getElementById('name');
     var userPhoto = document.getElementById('photo');
     var score = document.getElementsByClassName('scoreMenu');
     var text = document.getElementsByClassName('textInput');
-    var submit = document.getElementById('submit');
+    var subBox = document.getElementById('subBox');
     var userScore = [];
 
     var currentURL = window.location.origin;
     // this info can be sent from survey in app.js
-    document.querySelector('#del')
-        .addEventListener('click', function () {
-            var el = document.querySelector('.matchInfo');
-            el.parentNode.removeChild(el);
-        });
 
     submit.addEventListener('click', function (event) {
         event.preventDefault();
@@ -32,28 +25,25 @@ window.onload = function () {
         $.post("api/new", {
             userInput
         }, function (res) {
-            // $(".result").html(userScore);
             console.log(res);
             console.log('test');
             console.log(res.name);
-            var h3 = document.createElement("H3"); // Create a <h1> element
-            var match = document.getElementById('match');
-            var p = document.createElement("picture");
-            var img = document.createElement("img");
-            var btn = document.createElement("button");
-            var bt = document.createTextNode("DELETE");
+
+            var matchName = document.getElementById("matchName");
             var nt = document.createTextNode(res.name);
-            h3.setAttribute("id", "matchName");
-            h3.appendChild(nt);
-            match.appendChild(h3);
+            var match = document.getElementById('match');
+            var p = document.getElementById("matchPic");
+            var img = document.createElement("img");
+            var matchScore = document.getElementById("matchScore");
+            var st = document.createTextNode(res.scores);
+            var a = document.getElementById("again");
+            match.style.display = 'flex';
+
+            matchName.appendChild(nt);
             img.setAttribute("src", res.photo);
             p.appendChild(img);
-            p.setAttribute("id", "matchPhoto")
-            match.appendChild(p);
-            btn.appendChild(bt);
-            btn.setAttribute("id", "del");
-            match.appendChild(btn);
-
+            matchScore.appendChild(st);
+            a.style.display = 'flex';
             //document.getElementsByTagName("H1")[0].setAttribute("class", "democlass");
         }).done(function () {
             console.log('success');
@@ -61,5 +51,4 @@ window.onload = function () {
             alert("error");
         });
     });
-
 };
